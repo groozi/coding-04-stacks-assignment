724da4e1772223fd2e0a922dcb0ebcf37503868b
Graziella Buitron <graziella.buitron@g.austincc.edu>
2022-02-25 12:59:30 -0600

    Removed instructional comments in stack.h and reviewed spacing in stack.cpp

--- stack.h
+++ stack.h
@@ -8,48 +8,24 @@
 #ifndef STACK_H
 #define STACK_H
 
-/*
- * there are no other includes here because they are not needed.
- * if you do need other things included, they would go here
- */
-
 #include "data.h"
-//#include "main.h"
-#include <iostream> //delete this later
 
 #define MIN_SIZE 2  //minimum size for stack
 
 class Stack {
 
 public:
+    Stack(int);
+    ~Stack();
 
-    /* 
-     * write all your public method prototypes here
-     * this includes you constructor and destructor
-     */
-
-    //CONSTRUCTORS/DESTRUCTORS
-    Stack(int); //constructor receives int for size
-    ~Stack(); //destructor
-
-    //function prototypes
     bool push(int, string*);
     bool pop(Data*);
     bool peek(Data*);
     bool isEmpty();
-
     
 private:
-
-    /* 
-     * write all your private method prototypes here
-     */
-
-    // these are the only attributes you need.
-    // do not modify them or add any other attributes
     int top;
     int size;
-    //this is a pointer to a pointer that points to struct datas
     Data **stack;
 };
 
